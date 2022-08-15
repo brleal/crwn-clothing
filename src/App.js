26d33ce39c1bce8e00a59d94214f061d0ac11b59
brleal <brleal@gmail.com>
2022-08-15 06:55:22 -0300

    Init

--- src/App.js
+++ src/App.js
@@ -1,12 +1,52 @@
+import './categories.styles.scss'
 
 const App = () => {
-  return (
-    <div>
 
-      Hello
+    const categories = [
+        {
+            id: 1,
+            title: 'Hats',
+        },
+        {
+            id: 2,
+            title: 'Jackets',
+        },
+        {
+            id: 3,
+            title: 'Sneakers',
+        },
+        {
+            id: 4,
+            title: 'Womens',
+        },
+        {
+            id: 5,
+            title: 'Mens',
+        },
 
-    </div>
-  );
+    ];
+
+    return (
+        <div className='categories-container'>
+
+            {categories.map(({title, id}) => (
+                <div key={id} className="category-container">
+
+                    <div className='background-image'/>
+
+                    <div className='category-body-container'>
+
+                        <h2>{title}</h2>
+                        <p>Shop Now</p>
+
+                    </div>
+
+                </div>
+            ))}
+
+
+        </div>
+    );
 }
 
 export default App;
