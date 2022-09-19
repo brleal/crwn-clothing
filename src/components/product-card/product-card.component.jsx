f6c83a9ef9db10e7d7ced881caf53746b2df7d2f
brleal <brleal@gmail.com>
2022-09-19 06:54:34 -0300

    Aula 118 - Adicionar itens ao carrinho de compras.

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,10 +1,16 @@
-import './product-card.styles.scss';
-
+import {useContext} from "react";
 import Button from '../button/button.component';
+
+import {CartContext} from "../../contexts/cart.context";
+
+import './product-card.styles.scss';
 
 const ProductCard = ({product}) => {
 
     const {name, price, imageUrl} = product;
+    const {addItemToCart} = useContext(CartContext);
+
+    const addProductToCart = () => addItemToCart(product);
 
     return (
         <div className='product-card-container'>
@@ -13,7 +19,9 @@ const ProductCard = ({product}) => {
                 <span className='name'>{name}</span>
                 <span className='price'>{price}</span>
             </div>
-            <Button buttonType='inverted'>Adicionar ao carrinho</Button>
+            <Button buttonType='inverted' onClick={addProductToCart}>
+                Adicionar ao carrinho
+            </Button>
         </div>
     );
 }
